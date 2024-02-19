567d34958d34b9fd647a1a7ff13ac7001f300bf5
TMaxElectronics <60486146+TMaxElectronics@users.noreply.github.com>
2024-02-19 16:07:27 +0100

    Added power saving feature

--- ADC.c
+++ ADC.c
@@ -4,6 +4,8 @@
 #include "ADC.h"
 #include "FreeRTOSConfig.h"
 
+static void ADC_powerUp();
+static void ADC_powerDown();
 
 void ADC_init(ADC_OUTPUT_FORMAT_t format, uint32_t conversionTime_us){
     //set basic module options
@@ -15,9 +17,16 @@ void ADC_init(ADC_OUTPUT_FORMAT_t format, uint32_t conversionTime_us){
     if(clockScaler > 0xff) clockScaler = 0xff;
     AD1CON3 = ((autoSampleScaler & 0x1f) << 8) | (clockScaler & 0xff);
     AD1CHS = 0;
+    
+    //reduce power consumption as long as no conversion is running
+    ADC_powerDown();
 }
 
 uint32_t ADC_read(uint32_t channel){
+    //TODO add a semaphore to make sure that no conversions are started in parallel! (especially for conversions with more than one sample!)
+    
+    ADC_powerUp();
+    
     if(AD1CHSbits.CH0SA != channel){
         //switch channel
         AD1CHSbits.CH0SA = channel;
@@ -29,5 +38,19 @@ uint32_t ADC_read(uint32_t channel){
     AD1CON1SET = _AD1CON1_SAMP_MASK;
     while(!(AD1CON1 & _AD1CON1_DONE_MASK));
     
+    ADC_powerDown();
+    
     return ADC1BUF0;
+}
+
+static void ADC_powerDown(){
+    //is a conversion currently running? If so wait until its done
+    while(!(AD1CON1 & _AD1CON1_DONE_MASK));
+    
+    //power down module
+    AD1CON1bits.ON = 0;
+}
+
+static void ADC_powerUp(){
+    AD1CON1bits.ON = 1;
 }
